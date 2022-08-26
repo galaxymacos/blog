6aa6ff5375f64157b190ab197a353850da37ea86
xunruan <xunruan@icloud.com>
2022-08-26 16:29:06 -0400

    Add logging

--- lab/views.py
+++ lab/views.py
@@ -2,22 +2,28 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
+import logging
 
 
 # Create your views here.
 
 @csrf_exempt
 def twilio_webhook(request):
+    logging.basicConfig(filename='example.log', encoding='utf-8', level=logging.DEBUG)
     if request.method == 'POST':
+        logging.debug("Twilio webhook: POST received")
         return HttpResponse("Twilio webhook: POST received")
     else:
+        logging.debug("Twilio webhook: GET received")
         return HttpResponse("Twilio webhook: GET received")
 
 
 @csrf_exempt
 def cloudbeds_webhook(request):
-    # log
+    logging.basicConfig(filename='example.log', encoding='utf-8', level=logging.DEBUG)
     if request.method == "POST":
+        logging.debug("Cloudbeds webhook: POST received")
         return HttpResponse("Cloudbeds webhook: POST received")
     else:
+        logging.debug("Cloudbeds webhook: GET received")
         return HttpResponse("Cloudbeds webhook: GET received")
