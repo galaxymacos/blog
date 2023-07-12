e7d90e4d46acddee80f5049ae5c7b3b46ea5c6e9
Xun Ruan <xunruan@icloud.com>
2023-07-12 13:57:22 -0400

    modify structure

--- backend/Blog/settings.py
+++ backend/Blog/settings.py
@@ -66,7 +66,7 @@ ROOT_URLCONF = 'Blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [BASE_DIR / 'backend/templates']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
